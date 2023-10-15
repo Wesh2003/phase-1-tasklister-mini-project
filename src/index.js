96d002a8adeec98d3e12e6ebc84e0af35036d227
Tumaini Muriithi <wendomuriithi@gmail.com>
2023-10-15 18:03:32 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  function deleteTask() {
+    document.getElementById("tasks").delete();
+
+  } 
+
+  function newTask(){
+    let li = document.createElement('li');
+    li.innerText =  document.getElementById("new-task-description").value;
+    document.getElementById("tasks").appendChild(li);
+    if (Option.value === "high"){
+      document.querySelectorAll("li").color = "red";
+    }
+    else if (Option.value === "medium"){
+      document.querySelectorAll("li").color = "yellow";
+    }
+    else if (Option.value === "low"){
+      document.querySelectorAll("li").color = "green";
+    }
+
+    let del = document.createElement('BUTTON')
+    document.getElementById("tasks").appendChild(del);
+    function delTask() {
+      document.querySelector("li").delete()
+    }
+
+  }
+
+
 });
